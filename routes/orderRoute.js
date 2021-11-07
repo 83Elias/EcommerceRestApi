4aa165155e67cacd9e1fcb52527a9afded669ee8
Elias Vasquez <gamergy83@gmail.com>
2021-11-07 00:13:36 -0500

    finish ApiRest Ecommerce

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -61,33 +61,31 @@ router.get("/", verifyTokendAndAdmin, async (req, res) => {
 
 /* to-do  finish this function */
 
-router.get("/income",verifyTokendAndAdmin,async (req,res)=>{
-   const date= new Date();
-   const lastMonth=new Date(date.setMonth(date.getMonth()-1)); 
-   const previousMonth=new Date(new Date().setMonth(lastMonth.getMonth()-1));
+router.get("/income", verifyTokendAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 
-   try {
-        const income=await Order.aggregate([
-            {$match: {createdAt: {$gte: previousMonth}}},
-            {
-                $project:{
-                    month:{$month:"$createAt"},
-                    sales:"$amount",
-                },
-                {
-                    $group:{
-                        _id:"$month",
-                        total:{$sum:"$sales"},
-                    },
-                },
-                
-            },
-
-        ]);
-
-   } catch (err) {
-       res.status(500).json(err)
-   }
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
